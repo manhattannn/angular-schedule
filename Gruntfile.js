9579348c0b744c45f2b1810b5de7a82a67d0fe6b
Frank Carey <frank@zivtech.com>
2014-05-30 17:53:53 -0400

    Add bootstrap files to the grunt build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -314,6 +314,12 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
+        }, {
+          expand: true,
+          flatten: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>/fonts',
+          src: ['bower_components/sass-bootstrap/fonts/*.*']
         }]
       },
       styles: {
